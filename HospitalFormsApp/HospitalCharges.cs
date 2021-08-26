17430e1eaa1b266c97f837195143c362f8c8c7bd
Jared Whaley <jjw0008@auburn.edu>
2021-08-26 11:26:26 -0400

    Added "StayFee" property and the "CalcStayCharges" method.

--- HospitalFormsApp/HospitalCharges.cs
+++ HospitalFormsApp/HospitalCharges.cs
@@ -9,10 +9,16 @@ namespace HospitalFormsApp
     class HospitalCharges
     {
         public int Days { get; set; } = 0;
+        public int StayFee { get; private set; } = 0;
         public int RxFee { get; set; } = 0;
         public int SurgFee { get; set; } = 0;
         public int LabFee { get; set; } = 0;
         public int RehabFee { get; set; } = 0;
+
+        public void CalcStayCharges()
+        {
+            this.StayFee = this.Days * 350;
+        }
 
     }
 }
